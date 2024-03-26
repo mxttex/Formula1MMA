8030796e2f64fcd6d18e633d4ec4374cfd6aee37
ORIANO <andrea.baravelli.stud@ispascalcomandini.it>
2024-03-26 18:22:02 +0100

    ancoramento camera

--- scripts/gamePage.js
+++ scripts/gamePage.js
@@ -1,13 +1,17 @@
 $(document).ready(function(){
     var modalId = $("#modalId");
-            
-    modalId.addEventListener('show.bs.modal', function (event) {
-          // Button that triggered the modal
-          let button = event.relatedTarget;
-          // Extract info from data-bs-* attributes
-          let recipient = button.getAttribute('data-bs-whatever');
+    $(modalId).on('show.bs.modal',function(event){
+        let button= event.relatedTarget;
+        let recipient= button.getAttribute('data-bs-whatever')
+    });
+   
+    //  modalId.addEventListener('show.bs.modal', function (event) {
+    //       // Button that triggered the modal
+    //       let button = event.relatedTarget;
+    //       // Extract info from data-bs-* attributes
+    //       let recipient = button.getAttribute('data-bs-whatever');
 
-        // Use above variables to manipulate the DOM
-    })
+    //     // Use above variables to manipulate the DOM
+    // })
 });
     
